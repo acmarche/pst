25dd0e8dc84f08e4656a87b32169a83c1d880a50
Senechal jf <jfsenechal@gmail.com>
2025-09-04 13:25:25 +0200

    up

--- clean.sh
+++ clean.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 php artisan filament:clear-cached-components
 php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+php artisan route:clear
 rm -fr storage/logs/*.log
+rm -fr storage/framework/views/*.php
+
