56282f6b52c1bfb4a59e857a2fc91f0ca8e0a620
Senechal jf <jfsenechal@gmail.com>
2025-11-17 15:20:52 +0100

    no default value role name

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->enum('name', RoleEnum::toArray())->unique()->default(RoleEnum::AGENT->value);
+            $table->enum('name', RoleEnum::toArray())->unique()->nullable(false);
             $table->string('description')->nullable();
         });
 
