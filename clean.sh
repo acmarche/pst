58c4e6269321341039e5950c071edb8e789b4cef
Senechal jf <jfsenechal@gmail.com>
2025-11-18 08:29:22 +0100

    add --silent

--- clean.sh
+++ clean.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-php artisan filament:clear-cached-components
-php artisan cache:clear
-php artisan config:clear
-php artisan view:clear
-php artisan route:clear
-php artisan filament:optimize-clear
+php artisan filament:clear-cached-components --silent
+php artisan cache:clear --silent
+php artisan config:clear --silent
+php artisan view:clear --silent
+php artisan route:clear --silent
+php artisan filament:optimize-clear --silent
 rm -fr storage/logs/*.log
 rm -fr storage/framework/views/*.php
-php artisan filament:optimize
-
-
+php artisan filament:optimize --silent
