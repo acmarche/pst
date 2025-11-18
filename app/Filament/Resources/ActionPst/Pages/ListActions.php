739d2f9263590515bdbc917523ac47b9222eb1f8
Senechal jf <jfsenechal@gmail.com>
2025-11-18 11:02:18 +0100

    draft

--- app/Filament/Resources/ActionPst/Pages/ListActions.php
+++ app/Filament/Resources/ActionPst/Pages/ListActions.php
@@ -25,43 +25,67 @@ final class ListActions extends ListRecords
     public function getTabs(): array
     {
         $departmentSelected = UserRepository::departmentSelected();
-        $tabs = [
-            0 => Tab::make('All')
-                ->label('Toutes')
-                ->badge(function () use ($departmentSelected): int {
+
+        // Get selected filters
+        $filters = $this->tableFilters ?? [];
+
+
+        /*   ->badge(function () use ($departmentSelected): int {
                     return ActionRepository::findByDepartmentWithOosAndActions(
                         $departmentSelected
                     )->count();
+                })*/
+
+         /*  ->badge(function () use ($departmentSelected, $actionStateEnum): int {
+                        return ActionRepository::byStateAndDepartment($actionStateEnum, $departmentSelected)->count();
+                    })*/
+        $tabs = [
+            0 => Tab::make('All')
+                ->label('Toutes')
+                ->badge(function () use ($departmentSelected, $filters): int {
+                    $query = ActionRepository::findByDepartmentWithOosAndActions(
+                        $departmentSelected
+                    );
+
+                    return $this->applyFiltersToQuery($query, $filters)->count();
                 })
                 ->modifyQueryUsing(
-                    fn (Builder $query) => ActionRepository::findByDepartmentWithOosAndActions(
-                        $departmentSelected
+                    fn (Builder $query) => $this->applyFiltersToQuery(
+                        ActionRepository::findByDepartmentWithOosAndActions($departmentSelected),
+                        $filters
                     )
                 ),
         ];
         if (auth()->user()->hasRole(RoleEnum::ADMIN->value)) {
             $tabs[1] = Tab::make('ToValidate')
                 ->label('A valider')
-                ->badge(function (): int {
-                    return ActionRepository::toValidate()->count();
+                ->badge(function () use ($filters): int {
+                    $query = ActionRepository::toValidate();
+
+                    return $this->applyFiltersToQuery($query, $filters)->count();
                 })
                 ->badgeColor('warning')
                 ->icon('heroicon-m-exclamation-circle')
-                ->modifyQueryUsing(function (): Builder {
-                    return ActionRepository::toValidate();
+                ->modifyQueryUsing(function () use ($filters): Builder {
+                    return $this->applyFiltersToQuery(ActionRepository::toValidate(), $filters);
                 });
         }
         foreach (ActionStateEnum::cases() as $actionStateEnum) {
             $tabs[] =
                 Tab::make($actionStateEnum->value)
-                    ->badge(function () use ($departmentSelected, $actionStateEnum): int {
-                        return ActionRepository::byStateAndDepartment($actionStateEnum, $departmentSelected)->count();
+                    ->badge(function () use ($departmentSelected, $actionStateEnum, $filters): int {
+                        $query = ActionRepository::byStateAndDepartment($actionStateEnum, $departmentSelected);
+
+                        return $this->applyFiltersToQuery($query, $filters)->count();
                     })
                     ->label($actionStateEnum->getLabel())
                     ->badgeColor($actionStateEnum->getColor())
                     ->icon($actionStateEnum->getIcon())
-                    ->modifyQueryUsing(function (Builder $query) use ($actionStateEnum, $departmentSelected): Builder {
-                        return ActionRepository::byStateAndDepartment($actionStateEnum, $departmentSelected);
+                    ->modifyQueryUsing(function (Builder $query) use ($actionStateEnum, $departmentSelected, $filters): Builder {
+                        return $this->applyFiltersToQuery(
+                            ActionRepository::byStateAndDepartment($actionStateEnum, $departmentSelected),
+                            $filters
+                        );
                     });
         }
 
@@ -79,6 +103,48 @@ final class ListActions extends ListRecords
                 ->icon('tabler-list')
                 ->url(ActionPstResource::getUrl('asGoogleSheet')),
         ];
+    }
+
+    private function applyFiltersToQuery(Builder $query, array $filters): Builder
+    {
+        // Apply operational_objectives filter
+        if (isset($filters['operational_objectives']['value']) && $filters['operational_objectives']['value'] !== null) {
+            $query->where('operational_objective_id', $filters['operational_objectives']['value']);
+        }
+
+        // Apply state filter (skip for state-specific tabs)
+        if (isset($filters['state']['value']) && $filters['state']['value'] !== null) {
+            $query->where('state', $filters['state']['value']);
+        }
+
+        // Apply type filter
+        if (isset($filters['type']['value']) && $filters['type']['value'] !== null) {
+            $query->where('type', $filters['type']['value']);
+        }
+
+        // Apply isInternal filter
+        if (isset($filters['isInternal']['value']) && $filters['isInternal']['value'] !== null) {
+            $isInternal = $filters['isInternal']['value'] === 'true';
+            $query->whereHas('operationalObjective', function (Builder $q) use ($isInternal) {
+                $q->whereHas('strategicObjective', function (Builder $q) use ($isInternal) {
+                    $q->where('is_internal', $isInternal);
+                });
+            });
+        }
+
+        // Apply department filter
+        if (isset($filters['department']['value']) && $filters['department']['value'] !== null) {
+            $query->where('department', $filters['department']['value']);
+        }
+
+        // Apply users filter
+        if (isset($filters['users']['value']) && $filters['users']['value'] !== null) {
+            $query->whereHas('users', function (Builder $q) use ($filters) {
+                $q->where('users.id', $filters['users']['value']);
+            });
+        }
+
+        return $query;
     }
 
     private function debugQuery(): void
