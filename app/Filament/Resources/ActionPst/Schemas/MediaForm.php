d3dfef400eab4f7a7b00f02a435bad04c45f9d1c
jfsenechal <jfsenechal@gmail.com>
2026-01-14 11:21:10 +0100

    refactor MediaForm schema and model to align with updated field naming convention

--- app/Filament/Resources/ActionPst/Schemas/MediaForm.php
+++ app/Filament/Resources/ActionPst/Schemas/MediaForm.php
@@ -9,32 +9,30 @@ use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
-class MediaForm
+final class MediaForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema->schema(
             [
-                Hidden::make('file_mime'),
-                Hidden::make('file_size'),
-                TextInput::make('file_name')
+                Hidden::make('mime_type'),
+                Hidden::make('size'),
+                TextInput::make('name')
                     ->label('Nom du média')
                     ->required()
                     ->maxLength(150),
-                FileUpload::make('media')
+                FileUpload::make('file_name')
                     ->label('Pièce jointe')
                     ->required()
                     ->maxFiles(1)
                     ->disk('public')
                     ->directory('uploads')
-                    // ->acceptedFileTypes(['image/jpeg', 'image/png', 'application/pdf'])
-                    // ->preserveFilenames()
                     ->downloadable()
                     ->maxSize(10240)
                     ->afterStateUpdated(function ($state, Set $set) {
                         if ($state instanceof TemporaryUploadedFile) {
-                            $set('file_mime', $state->getMimeType());
-                            $set('file_size', $state->getSize());
+                            $set('mime_type', $state->getMimeType());
+                            $set('size', $state->getSize());
                         }
                     }),
             ]
