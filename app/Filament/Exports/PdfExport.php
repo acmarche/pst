d8d3f9b2686cb7910dc921968fedacd2e01f46eb
jfsenechal <jfsenechal@gmail.com>
2026-01-28 16:35:41 +0100

    remove logging of `PdfExport::exportAction` config values for cleanup and clarity

--- app/Filament/Exports/PdfExport.php
+++ app/Filament/Exports/PdfExport.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Exports;
 
 use App\Models\Action;
-use Illuminate\Support\Facades\Log;
 use Spatie\Browsershot\Browsershot;
 use Spatie\LaravelPdf\Facades\Pdf;
 use Spatie\LaravelPdf\PdfBuilder;
@@ -12,17 +11,9 @@ final class PdfExport
 {
     public static function exportAction(Action $action): PdfBuilder
     {
-        if ($path = config('pdf.node_modules_path')) {
-            Log::error('JFS node_modules_path'.$path);
-        }
-        if ($path = config('pdf.chrome_path')) {
-            Log::error('JFS chrome_path'.$path);
-        }
-
         return Pdf::html(view('pdf.action', [
             'action' => $action,
         ]))
-
             ->withBrowsershot(function (Browsershot $browsershot): void {
                 if ($path = config('pdf.node_modules_path')) {
                     $browsershot->setNodeModulePath($path);
