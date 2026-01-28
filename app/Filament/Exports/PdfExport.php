bc83129b6fb60926b6094d756d6aa79e50f16ad4
jfsenechal <jfsenechal@gmail.com>
2026-01-28 16:10:16 +0100

    fix typo in method name and configure `Browsershot` node module path in `PdfExport::exportAction`

--- app/Filament/Exports/PdfExport.php
+++ app/Filament/Exports/PdfExport.php
@@ -3,19 +3,22 @@
 namespace App\Filament\Exports;
 
 use App\Models\Action;
+use Spatie\Browsershot\Browsershot;
 use Spatie\LaravelPdf\Facades\Pdf;
 use Spatie\LaravelPdf\PdfBuilder;
 
 final class PdfExport
 {
-    public static function exportAcion(Action $action): PdfBuilder
+    public static function exportAction(Action $action): PdfBuilder
     {
         return Pdf::html(view('pdf.action', [
             'invoiceNumber' => '1234',
             'customerName' => 'Grumpy Cat',
             'action' => $action,
         ]))
-            // ->withBrowsershot(fn(Browsershot $shot) => $shot->setNodeBinary()->setNpmBinary()->setPuppeteerBinary()->setPuppeteerLaunchOptions([]))
+            ->withBrowsershot(fn (Browsershot $browsershot) => $browsershot
+                ->setNodeModulePath('/var/www/puppeteer/node_modules')
+            )
             ->download('action-'.$action->id.'.pdf');
         // ->save('action-'.$action->id.'.pdf');
     }
