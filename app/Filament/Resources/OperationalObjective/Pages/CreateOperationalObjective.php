3a97b78ac94bdef81d06b337f0b9ef988a1b1310
Senechal jf <jfsenechal@gmail.com>
2026-02-02 11:51:53 +0100

    feat: add warning notification and redirect logic to restrict direct creation of Operational Objectives

--- app/Filament/Resources/OperationalObjective/Pages/CreateOperationalObjective.php
+++ app/Filament/Resources/OperationalObjective/Pages/CreateOperationalObjective.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\OperationalObjective\Pages;
 
 use App\Filament\Resources\OperationalObjective\OperationalObjectiveResource;
+use App\Filament\Resources\StrategicObjective\StrategicObjectiveResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 final class CreateOperationalObjective extends CreateRecord
@@ -10,6 +12,18 @@ final class CreateOperationalObjective extends CreateRecord
     protected static string $resource = OperationalObjectiveResource::class;
 
     protected static bool $canCreateAnother = false;
+
+    public function mount(): void
+    {
+        Notification::make()
+            ->warning()
+            ->title('Création non autorisée')
+            ->body('Un objectif opérationnel doit être créé depuis la page de visualisation d\'un objectif stratégique.')
+            ->persistent()
+            ->send();
+
+        $this->redirect(StrategicObjectiveResource::getUrl('index'));
+    }
 
     public function getTitle(): string
     {
