bb6cb2dfb728cc009020de1580fc28a66b759284
Senechal jf <jfsenechal@gmail.com>
2026-02-03 11:52:12 +0100

    feat: display total count in `ListOperationalObjectives` title

--- app/Filament/Resources/OperationalObjective/Pages/ListOperationalObjectives.php
+++ app/Filament/Resources/OperationalObjective/Pages/ListOperationalObjectives.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources\OperationalObjective\Pages;
 use App\Filament\Resources\OperationalObjective\OperationalObjectiveResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Support\Htmlable;
 
 final class ListOperationalObjectives extends ListRecords
 {
     protected static string $resource = OperationalObjectiveResource::class;
+
+    public function getTitle(): string|Htmlable
+    {
+        return $this->getAllTableRecordsCount().' Objectifs Opérationnels (OO)';
+    }
 
     protected function getHeaderActions(): array
     {
